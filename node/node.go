360c371377eebfc7d0037c98082e39bf6b6cd2f5
Rahul <rahulsamp4@gmail.com>
2025-08-19 00:08:01 -0700

    bidirectionality added using channels

--- node/node.go
+++ node/node.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"sync"
 )
 
 func main() {
@@ -17,14 +18,52 @@ func main() {
 	defer conn.Close()
 	log.Println("Connected, Say Hi!")
 
+	// we are kind of type casting conn to *net.TCPConn so we can use its methods even if we do not get it from net.dial
+	tcp, _ := conn.(*net.TCPConn)
+
+	// var wg sync.WaitGroup
+	// wg.Add(2) // we dont need wait groups as we have decided to use channels instead
+
+	//empty structs need 0 memory so this is a lightweight way to signal done
+	done := make(chan struct{})
+	var once sync.Once
+	signal := func() { once.Do(func() { close(done) }) }
+
+	//stdin -> conn
 	go func() {
+		// io.copy is a blocking code
 		if _, err := io.Copy(conn, os.Stdin); err != nil {
 			log.Println("write error: ", err)
 		}
+		// signal no more writes from this side
+		if tcp != nil {
+			_ = tcp.CloseWrite()
+		}
+		signal() // tell main that one direction finished -> chamnnel
 	}()
 
-	// this has to be below the above go function, as this will block the thread
-	if _, err := io.Copy(os.Stdout, conn); err != nil {
-		log.Fatal(err)
-	}
+	// conn -> stdout
+	go func() {
+		// defer wg.Done()
+		if _, err := io.Copy(os.Stdout, conn); err != nil {
+			log.Fatal(err)
+		}
+		// optional: stop further reads
+		if tcp != nil {
+			_ = tcp.CloseRead()
+		}
+		signal() // tell main that one direction finished
+	}()
+
+	// // this has to be below the above go function, as this will block the thread
+	// now code changed and no need of below
+	// if _, err := io.Copy(os.Stdout, conn); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	//wg.Wait() // not needed since we are using channels
+
+	// if we do not have this, then main function wont wait for go-routines to close and shuts off, whihc also closes other go routines thus
+	// ending the program
+	<-done
 }
